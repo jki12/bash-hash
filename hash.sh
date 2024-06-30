da1f2f990f98719222784dd6436a6d123de3a1ad
jki12 <sjak0227@naver.com>
2024-06-30 23:55:51 +0900

    feat: hash.sh

---
+++ hash.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+cat << "EOF" # heredoc
+ _               _           _     
+| |             | |         | |    
+| |__   __ _ ___| |__    ___| |__  
+| '_ \ / _` / __| '_ \  / __| '_ \ 
+| | | | (_| \__ \ | | |_\__ \ | | |
+|_| |_|\__,_|___/_| |_(_)___/_| |_|
+
+EOF
+
+echo "+----------------------------------+"
+file_list=(`ls`)
+len=${#file_list[@]} # file list length.
+
+hash_list=('md5' 'sha1')
+for i in `seq -w 0 $((len - 1))` # script 파일 제외, 범위를 [0, len - 1)로 하기 위해.
+do
+	if [[ "${file_list[i]}" == "hash.sh" ]]; then
+		continue
+	fi
+
+	file_name=$(echo "${file_list[i]}" | cut -d '.' -f1)
+	for hash in ${hash_list[@]}
+	do
+	hash_file_name="${file_name}.${hash}"
+	printf "| %-*s |\n" 32 "created ${hash_file_name}"
+	echo `openssl ${hash} ${file_list[i]}` | cut -d ' ' -f2 > ${hash_file_name}
+	done
+done
+echo "+----------------------------------+"
