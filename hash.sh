2b93fffb1cb1fe34777f84cc9228959671fb3b35
jki12 <sjak0227@naver.com>
2024-07-03 11:49:51 +0900

    fix: 변수 선언 위치 수정, 파일 이름에 공백 들어가면 생기는 이슈 해결

--- hash.sh
+++ hash.sh
@@ -10,15 +10,13 @@ cat << "EOF" # heredoc
 
 EOF
 
+hash_list=('md5' 'sha1')
 max_len=32;
 
 echo "+----------------------------------+"
-file_list=(`ls -p | grep -v "/"`) # dir 파일 제외.
-
-hash_list=('md5' 'sha1')
-for file in "${file_list[@]}"
+find . -type f ! -name '.*' -print0 |  while IFS= read -d "" file
 do
-	if [[ "${file}" == "hash.sh" ]]; then
+	if [[ "${file}" == "./hash.sh" ]]; then
 		continue
 	fi
 
@@ -26,7 +24,7 @@ do
 	do
 		hash_file="${file}.${hash}"
 		printf "| %-*s |\n" ${max_len} "created ${hash_file}"
-		echo `openssl ${hash} ${file}` | cut -d ' ' -f2 > ${hash_file}
+		echo `openssl ${hash} "${file}"` | cut -d ' ' -f2 > "${hash_file}"
 	done
 done
 echo "+----------------------------------+"
